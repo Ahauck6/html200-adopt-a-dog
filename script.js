18d0897256d6a0f41b58558f1a74a06997cba90b
unknown <Ahauck6@uw.edu>
2023-11-29 12:18:10 -0800

    Add click handler

---
+++ script.js
@@ -0,0 +1,58 @@
+function murphyDog() {
+    alert("Murphy\nBreed: Boxer Mix\nCost to Adopt: $123.45")
+}
+
+function poppyDog() {
+    alert("Poppy\nBreed: Shih Tzu\nCost to Adopt: $123.45")
+}
+
+function jackDog() {
+    alert("Jack\nBreed: Beagle\nCost to Adopt: $123.45")
+}
+
+function duffyDog() {
+    alert("Duffy\nBreed: Pit Mix\nCost to Adopt: $123.45")
+}
+
+function lucasDog() {
+    alert("Lucas\nBreed: Black Lab\nCost to Adopt: $123.45")
+}
+
+function jakeDog() {
+    alert("Jake\nBreed: Golden Retriever\nCost to Adopt: $123.45")
+}
+
+function angusDog() {
+    alert("Angus\nBreed: Boxer\nCost to Adopt: $123.45")
+}
+
+function violetDog() {
+    alert("Violet\nBreed: Chocolate Lab\nCost to Adopt: $123.45")
+}
+
+function piperDog() {
+    alert("Piper\nBreed: Long Haired Lab\nCost to Adopt: $123.45")
+}
+
+function maximusDog() {
+    alert("Maximus\nBreed: Terrier Mix\nCost to Adopt: $123.45")
+}
+
+function lunaDog() {
+    alert("Luna\nBreed: Yellow Lab\nCost to Adopt: $123.45")
+}
+
+function stellaDog() {
+    alert("Stella\nBreed: Chihuahua\nCost to Adopt: $123.45")
+}
+
+
+let adoptionTotal = 0
+
+function adoptionCost() {
+    adoptionTotal = adoptionTotal + 123.45
+    alert("$" + adoptionTotal)
+}
+
+
+
